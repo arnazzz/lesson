2c510f5400b4750785fa891cdfd0250acff39ad3
arnazzz <gabbasarnaz@gmail.com>
2025-02-23 20:13:58 +0500

    resolved half tasks

--- script1.js
+++ script1.js
@@ -1,16 +1,54 @@
 // Задача 1. Запросить у пользователя два числа, сложить их и вывести результат.
 
-
+// const x = Number(prompt('type text 1'));
+// const y = Number(prompt('type text 2'));
+// alert(x + y)
 
 
 // Задача 2. Запросить у пользователя число и определить, положительное оно, отрицательное или ноль.
 // if, else, else if / Тернарный оператор (? :) / switch...case / ??
+
+// const x = Number(prompt('type text 1...'));
+// if (x > 0) {
+//   alert('Положительно')
+// } else if (x < 0) {
+//   alert('Отрицательно')
+// } else {
+//   alert('Вы ввели ноль')
+// };
+
+// const x = Number(prompt('type text 1...'));
+// x > 0 ? alert('Положительно') : x < 0 ? alert('Отрицательно') : alert('Вы ввели ноль');
+
+// const x = Number(prompt('type text 1...'));
+// switch(true){
+//   case x > 0:
+//   alert('Положительно');
+//   break;
+//   case x < 0:
+//   alert('Отрицательно');
+//   break;
+//   default:
+//   alert('Вы ввели ноль');
+// }
+
+
 
 
 
 // Задача 3. Вывести числа от 1 до 10 в браузер.
 // for / while / do...while / break / continue
 
+// for (let i = 0; i < 11; i++){
+//   alert(i)
+// };
+
+// let count = 1;
+
+// while (count < 11) {
+//   console.log(count);
+//   count++;
+// }
 
 
 // Задача 4. Написать функцию, которая возвращает большее из двух чисел.
